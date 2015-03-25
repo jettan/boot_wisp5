9790ee69add57bf4267379df98cce79692e68d5d
Jethro Tan <jethrotan20@gmail.com>
2015-03-25 22:27:54 +0100

    Unbreak R1000.

--- CCS/wisp-base/rfid/RX_ISR.asm
+++ CCS/wisp-base/rfid/RX_ISR.asm
@@ -83,17 +83,16 @@ badDelim:									; Go Back To Sleep.
 	;*********************************************************************************************************************************
 goodDelim:                                ;[23]
 	; TODO The following shouldn't overwrite other bits in PRXSEL!?
-	BIS.B	#PIN_RX, &PRXSEL0	          ;[5] Enable Timer0A0
-	BIC.B	#PIN_RX, &PRXSEL1	  		  ;[5] Enable Timer0A0
-	CLR.B	&PRXIE					      ;[4] Disable the Port 1 Interrupt
-	BIC		#(SCG1), 0(SP)		    	  ;[5] Enable the DCO to start counting
-	PUSHM.A #1,	R15                       ;[2] Backup data of R15.
-	;MOV	#FORCE_SKIP_INTO_RTCAL, R15   ;[2] Set R15 to 24.
-	MOV		#(10), R15                    ;[2] Set R15 to 9.
+	BIS.B   #PIN_RX, &PRXSEL0             ;[5] Enable Timer0A0
+	BIC.B   #PIN_RX, &PRXSEL1             ;[5] Enable Timer0A0
+	CLR.B   &PRXIE                        ;[4] Disable the Port 1 Interrupt
+	BIC     #(SCG1), 0(SP)                ;[5] Enable the DCO to start counting
+	PUSHM.A #1, R15                       ;[2] Backup data of R15.
+	MOV     #FORCE_SKIP_INTO_RTCAL, R15   ;[2] Set R15 to 24.
 
-delimDelay:                               ;[n*4 + 7] (n = 10 to make this 1 Tari = 7.14 us)
-	DEC		R15                           ;[2] Executed n times.
-	JNZ		delimDelay                    ;[2] Executed n times.
+delimDelay:                               ;[103]
+	DEC		R15                           ;[2] Executed 24 times.
+	JNZ		delimDelay                    ;[2] Executed 24 times.
 	POPM.A #1,	R15                       ;[2] Restore data of R15.
 
 	; Moved to here because the timer0A0 interrupt should not fire at falling edge of delay cycle which happen after Good Delimiter
@@ -101,19 +100,19 @@ delimDelay:                               ;[n*4 + 7] (n = 10 to make this 1 Tari
 
 startupT0A0_ISR:                          ;[21] cycles = 1.3125 us
 	BIC		#CCIFG, &TA0CCTL0		      ;[5] Clear the interrupt flag for Timer0A0 Compare
-	CLR		&TA0R					      ;[4] Reset TAR value
+	CLR		&TA0R					      ;[4] ***Reset clock!***
 	CLR		&(rfid.edge_capture_prev_ccr) ;[4] Clear previous value of CCR capture
-	CLR		&(rfid.edge_capture_prev_ccr) ;[4] Reset previous edge capture time
+	CLR		&(rfid.edge_capture_prev_ccr) ;[4] Reset previous edge capture time TODO: Is this needed since it's already been done in the line above?
 	CLR.B	&PRXIFG					      ;[4] Clear the Port 1 flag.
 
 
-	;ADD 	#36, &TA0R				      ;[5] The modified code seem to add some commands that increase the amount of waiting after finding the Good Delimiter.
+	ADD 	#36, &TA0R				      ;[5] The modified code seem to add some commands that increase the amount of waiting after finding the Good Delimiter.
 	;;We just need to wait for 1 Tari (7.14 us for R1000 and 6.25 us for R420!) which is equal to 100 cycles for R420 and 114-115 cycles for R1000 on 8MHz before counting the length of RTcal but instead in this code we wait for about
-	;;86 cycles and after that clear the TA0R. This is the reason we added #36 to TA0R.
+	;;86 cycles (?) and after that clear the TA0R. This is the reason we added #36 to TA0R.
 
 	RETI                                  ;[5]
 
-	;; We are already 153 cycles (9.5625 us) in since delimiter.
+	;; We are 153 cycles (9.5625 us) past delimiter.
 	;	//Now wait into data0 somewhere!
 
 ;*************************************************************************************************************************************
