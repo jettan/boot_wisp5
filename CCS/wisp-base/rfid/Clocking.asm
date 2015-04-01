cf512c3a8ac6a72583c43ed270a285cb3bde4f0b
Jethro Tan <jethrotan20@gmail.com>
2015-04-01 19:22:37 +0200

    Explain Clocking.asm.

--- CCS/wisp-base/rfid/Clocking.asm
+++ CCS/wisp-base/rfid/Clocking.asm
@@ -4,7 +4,6 @@
 ;*	@author		Saman Naderiparizi, UW Sensor Systems Lab
 ;*	@created	3-10-14
 ;*
-;*
 ;*	@section	Command Handles
 ;*				-#TxClock , RxClock
 ;*/
@@ -14,26 +13,25 @@
     .cdecls C,LIST, "rfid.h"
 	.def  TxClock, RxClock
 
-;;; Switch to TX frequency (10.5 MHz)
+; TX clock frequency: 12 MHz.
 TxClock:
-	MOV.B           #(0xA5), &CSCTL0_H                                            ;CSCTL0_H = 0xA5
-	MOV.W           #(DCORSEL|DCOFSEL_6), &CSCTL1 ;
-	MOV.W           #(SELA_0|SELM_3), &CSCTL2     ;
-	BIS.W           #(SELS_3), &CSCTL2
-	MOV.W           #(DIVA_0|DIVS_1|DIVM_1), &CSCTL3 ;
-	BIS.W           #(MODCLKREQEN|SMCLKREQEN|MCLKREQEN|ACLKREQEN), &CSCTL6
+	MOV.B           #(0xA5), &CSCTL0_H                                            ; Write CSKEY password.
+	MOV.W           #(DCORSEL|DCOFSEL_6), &CSCTL1                                 ; Set clocks to 24 MHz.
+	MOV.W           #(SELA_0|SELM_3), &CSCTL2                                     ; Select  LFXTCTL as ACLK source and DCOCLK as MCLK source.
+	BIS.W           #(SELS_3), &CSCTL2                                            ; Select DCOCLK as SMCLK source.
+	MOV.W           #(DIVA_0|DIVS_1|DIVM_1), &CSCTL3                              ; Don't divide ACLK. Divide MCLK and SMCLK by 2 (12 MHz).
+	BIS.W           #(MODCLKREQEN|SMCLKREQEN|MCLKREQEN|ACLKREQEN), &CSCTL6        ; Enable clock requests.
 
 	RETA
 
-
-;;; Switch to RX frequency (16 MHz)
+; RX clock frequency: 16 MHz.
 RxClock:
-	MOV.B           #(0xA5), &CSCTL0_H                                            ;CSCTL0_H = 0xA5
-	MOV.W           #(DCORSEL|DCOFSEL_4), &CSCTL1                                 ;CSCTL1 = DCORSEL + DCOFSEL_4
-	MOV.W           #(SELA_0|SELM_3), &CSCTL2                                     ;CSCTL2 = SELA_0 + SELM_3
-	BIS.W           #(SELS_3), &CSCTL2                                            ;CSCTL2 |= SELS_3
-	MOV.W           #(DIVA_0|DIVS_0|DIVM_0), &CSCTL3                              ;CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0
-	BIS.W           #(MODCLKREQEN|SMCLKREQEN|MCLKREQEN|ACLKREQEN), &CSCTL6
+	MOV.B           #(0xA5), &CSCTL0_H                                            ; Write CSKEY password.
+	MOV.W           #(DCORSEL|DCOFSEL_4), &CSCTL1                                 ; Set clocks to 16 MHz.
+	MOV.W           #(SELA_0|SELM_3), &CSCTL2                                     ; Select  LFXTCTL as ACLK source and DCOCLK as MCLK source.
+	BIS.W           #(SELS_3), &CSCTL2                                            ; Select DCOCLK as SMCLK source.
+	MOV.W           #(DIVA_0|DIVS_0|DIVM_0), &CSCTL3                              ; Don't divide clock.
+	BIS.W           #(MODCLKREQEN|SMCLKREQEN|MCLKREQEN|ACLKREQEN), &CSCTL6        ; Enable clock requests.
 	
 	RETA
 
