a38125b75048aa0510342a81fbddf2639dbc424f
Jethro Tan <jethrotan20@gmail.com>
2015-06-12 15:01:20 +0200

    Delete Debug folder.

--- CCS/bsl/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"/home/dutiir/workspace/boot_wisp5/CCS/wisp-base/build/wisp-base.lib" -l"libmath.a"
-
